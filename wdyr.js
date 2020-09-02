d1fbea2ccc7b2c2977675e92c720e515d765b5f6
zhangcx01 <zhangcx01@mail.jj.cn>
2020-09-02 16:55:21 +0800

    关闭警告

--- wdyr.js
+++ wdyr.js
@@ -3,6 +3,7 @@ import React from 'react';
 // eslint-disable-next-line no-undef
 if (__DEV__) {
   // eslint-disable-next-line global-require
+  // eslint-disable-next-line import/no-extraneous-dependencies
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React);
 }
